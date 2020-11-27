cc465ffb4c2ecaee5a95bb2f4082a0b927fa74ed
Fube <fubeitch@gmail.com>
2020-11-27 18:36:04 -0500

    Added ContactManager.cs

--- ContactManager.cs
+++ ContactManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,9 @@ namespace TermProject
 {
     sealed class ContactManager
     {
+        private static int _count = 0;
 
-        private Dictionary<int, Contact> _contacts;
+        private readonly Dictionary<int, Contact> _contacts;
 
         private static readonly Lazy<ContactManager> LazyInstance = new Lazy<ContactManager>(() => new ContactManager());
 
@@ -23,24 +25,54 @@ namespace TermProject
             _contacts = new Dictionary<int, Contact>();
         }
 
-        public void AddContact(int id, Contact contact) => _contacts.Add(id, contact);
-
-        public void RemoveContact(int id) => _contacts.Remove(id);
-
-        public void ImportFromFile(int startingId, Stream stream)
+        public int AddContact(Contact contact)
         {
-            using (TextFieldParser parser = new TextFieldParser(stream))
+            _contacts.Add(++_count, contact);
+            return _count;
+        }
+
+        public bool RemoveContact(int id)
+        {
+            try
+            {
+                _contacts.Remove(id);
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.Message);
+                Trace.WriteLine(e.StackTrace);
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ImportFromFile(Stream stream)
+        {
+            using (var parser = new TextFieldParser(stream))
             {
                 parser.TextFieldType = FieldType.Delimited;
                 parser.SetDelimiters(",");
 
-                for (int id = startingId; !parser.EndOfData; id++)
+                while(!parser.EndOfData)
                 {
                     string[] fields = parser.ReadFields();
-                    AddContact(id, new Contact(fields[0], fields[2], fields[3]??));
+                    if (fields != null)
+                        AddContact(new Contact(fields[0], fields[2], fields.Length < 3 ? null : fields[3]));
                 }
             }
         }
 
+        public void ExportToFile(string path)
+        {
+            using (var sw = new StreamWriter(path))
+            {
+                foreach (var v in _contacts.Select(kvp => kvp.Value))
+                {
+                    sw.WriteLine($"{v.FirstName},{v.LastName}{(v.PhoneNumber == null ? "" : $",{v.PhoneNumber}")}");
+                    sw.Flush();
+                }
+            }
+        }
     }
 }
